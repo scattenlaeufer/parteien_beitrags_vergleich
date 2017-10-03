ecd9c828f20332d672cb8be5aca661ee7f702b0e
Björn Guth <bjoern.guth@rwth-aachen.de>
2017-10-03 18:39:06 +0200

    added SPD

--- README.md
+++ README.md
@@ -3,7 +3,17 @@ Ein kleiner Vergleich der Mitgliedsbeiträge einiger deutschen Parteien
 
 ![Mitgliedsbeiträge als Graph](parteienbeitrag.png)
 
+## Anmerkungen
+
+### SPD
+Die SPD teilt die Mitgliedsbeiträge in fünf Kategorien nach
+Monatsnettoeinkommen auf. ab der zweiten Kategorie werden diese nochmal in
+jeweils drei Beitragsstufen aufgeteilt, in die sich jedes Mitglied entsprechend
+Einkommen einstuft. Diese Beitragsstufen sind im Graphen durch die gestrichelte
+Line verdeutlicht.
+
 ## Quelle
 Partei | Quelle | gültig seit
 -------|--------|------------
 DIE LINKE | [Beitragstabelle der Partei DIE LINKE](https://www.die-linke.de/fileadmin/download/grundsatzdokumente/beitragstabelle/die_linke_beitragstabelle_stand_januar_2016.pdf) | 01.01.2016
+SPD | [Finanzordnung der Sozialdemokratischen Partei Deutschlands](https://www.spd.de/fileadmin/Dokumente/Parteiorganisation/Organisationsstatut.pdf) | 26.01.2016
--- parteien_beitrags_verleich.py
+++ parteien_beitrags_verleich.py
@@ -41,11 +41,66 @@ def die_linke(einkommen):
         beitrag.append(b + 0.5)
     return beitrag
 
+
+def spd(einkommen):
+    beitrag = []
+    for e in einkommen:
+        if e == 0:
+            beitrag.append(2.5)
+        elif e <= 1000:
+            beitrag.append(5)
+        elif e <= 2000:
+            beitrag.append(7.5)
+        elif e <= 3000:
+            beitrag.append(25)
+        elif e <= 4000:
+            beitrag.append(45)
+        else:
+            beitrag.append(100)
+    return beitrag
+
+
+def spd_2(einkommen):
+    beitrag = []
+    for e in einkommen:
+        if e == 0:
+            beitrag.append(2.5)
+        elif e <= 1000:
+            beitrag.append(5)
+        elif e <= 1000 + 1000 / 3:
+            beitrag.append(7.5)
+        elif e <= 1000 + 2000 / 3:
+            beitrag.append(15)
+        elif e <= 2000:
+            beitrag.append(20)
+        elif e <= 2000 + 1000 / 3:
+            beitrag.append(25)
+        elif e <= 2000 + 2000 / 3:
+            beitrag.append(30)
+        elif e <= 3000:
+            beitrag.append(35)
+        elif e <= 3000 + 1000 / 3:
+            beitrag.append(45)
+        elif e <= 3000 + 2000 / 3:
+            beitrag.append(60)
+        elif e <= 4000:
+            beitrag.append(75)
+        elif e <= 4000 + 1000 / 3:
+            beitrag.append(100)
+        elif e <= 4000 + 2000 / 3:
+            beitrag.append(150)
+        else:
+            beitrag.append(250)
+    return beitrag
+
+
 def make_graph():
     # einkommen = [e for e in range(2500)]
-    einkommen = numpy.arange(0, 3000, 1)
+    einkommen = numpy.arange(0, 5000, 1)
     pyplot.figure()
-    pyplot.plot(einkommen, die_linke(einkommen), 'r')
+    pyplot.plot(einkommen, die_linke(einkommen), 'k',
+                einkommen, spd_2(einkommen), 'r--',
+                einkommen, spd(einkommen), 'r')
     # pyplot.show()
     pyplot.savefig('parteienbeitrag.png', bbox_inches='tight')
 
